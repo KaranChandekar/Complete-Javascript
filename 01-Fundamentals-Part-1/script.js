8973ec7017d9514c6d2ea9243e64d659ad744bdf
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-07 14:17:50 +0530

    Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,19 @@
-let js = "amazing";
-if (js === "amazing") {
-  alert("Javascript is FUN!");
-}
-console.log(45 + 55 + 20 - 30);
+////////////////////////////////////
+// Linking a JavaScript File
+// let js = "amazing";
+// if (js === "amazing") {
+//   alert("Javascript is FUN!");
+// }
+// console.log(45 + 55 + 20 - 30);
+
+
+////////////////////////////////////
+// Values and Variables
+console.log("Karan");
+console.log(18);
+
+let firstName = "Karan";
+let lastName = "Chandekar"
+console.log(firstName);
+console.log(lastName);
+console.log(firstName);
