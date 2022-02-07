9d81df67676c0b495ff23bbfbe0066d53213395b
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-07 14:19:50 +0530

    Variable name conventions

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -9,11 +9,26 @@
 
 ////////////////////////////////////
 // Values and Variables
-console.log("Karan");
-console.log(18);
+// console.log("Karan");
+// console.log(18);
 
-let firstName = "Karan";
-let lastName = "Chandekar"
-console.log(firstName);
-console.log(lastName);
-console.log(firstName);
+// let firstName = "Karan";
+// let lastName = "Chandekar"
+// console.log(firstName);
+// console.log(lastName);
+// console.log(firstName);
+
+// Variable name conventions
+let karan_chandekar = "KC";
+let $function = 18;
+
+let person = "Karan";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Developer";
+
+let job1 = "programmer";
+let job2 = "developer";
+
+console.log(myFirstJob);
