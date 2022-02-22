9c72951f9fc70c1892404dc921a84a44fc13f7d7
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-22 13:26:30 +0530

    loging the typeof the variable

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -37,3 +37,20 @@
 // Data Types
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
