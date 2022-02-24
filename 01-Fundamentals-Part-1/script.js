e099bd7ade906a958a0fb8c123c5cd9a08429c4d
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-24 20:28:15 +0530

    let, const and var

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -6,7 +6,6 @@
 // }
 // console.log(45 + 55 + 20 - 30);
 
-
 ////////////////////////////////////
 // Values and Variables
 // console.log("Karan");
@@ -43,14 +42,29 @@ console.log(typeof javascriptIsFun);
 // console.log(typeof 23);
 // console.log(typeof 'Jonas');
 
-javascriptIsFun = 'YES!';
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991;
-console.log(typeof year);
+// year = 1991;
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+////////////////////////////////////
+// let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Schmedtmann";
+console.log(lastName);
