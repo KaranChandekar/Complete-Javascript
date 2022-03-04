5d04555a51fce39086fe890e3f1d544a59ac3609
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-04 13:15:27 +0530

    equality operators

---
+++ 01-Fundamentals-Part-1/equality-operators.js
@@ -0,0 +1,23 @@
+////////////////////////////////////
+// Equality Operators: == vs. ===
+const age = "18";
+if (age === 18) console.log("You just became an adult :D (strict)");
+
+if (age == 18) console.log("You just became an adult :D (loose)");
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  // 22 === 23 -> FALSE
+  console.log("Cool! 23 is an amzaing number!");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+} else if (favourite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7 or 9");
+}
+
+if (favourite !== 23) console.log("Why not 23?");
