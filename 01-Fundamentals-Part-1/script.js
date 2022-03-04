689cdd9c1b19000a8c494def5122cb568dca3bc8
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-04 10:12:20 +0530

    add file for values and variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -8,28 +8,6 @@
 
 
 ////////////////////////////////////
-// Data Types
-// let javascriptIsFun = true;
-// console.log(javascriptIsFun);
-
-// console.log(typeof true);
-// console.log(typeof javascriptIsFun);
-// console.log(typeof 23);
-// console.log(typeof 'Jonas');
-
-// javascriptIsFun = 'YES!';
-// console.log(typeof javascriptIsFun);
-
-// let year;
-// console.log(year);
-// console.log(typeof year);
-
-// year = 1991;
-// console.log(typeof year);
-
-// console.log(typeof null);
-
-////////////////////////////////////
 // let, const and var
 // let age = 30;
 // age = 31;
