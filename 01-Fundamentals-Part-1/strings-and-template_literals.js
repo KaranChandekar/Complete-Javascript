2ce73b7b787d37161744032e35a3a5386f16cd84
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-04 10:43:07 +0530

    string and template literals

---
+++ 01-Fundamentals-Part-1/strings-and-template_literals.js
@@ -0,0 +1,24 @@
+////////////////////////////////////
+// Strings and Template Literals
+
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log("String with \n\
+multiple \n\
+lines");
+
+console.log(`String
+multiple
+lines`);
