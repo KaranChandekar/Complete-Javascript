5a0f22a392a04ee9496c3d1e88c0b2df63bf39c4
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-04 10:17:34 +0530

    modify script file to link file

--- 01-Fundamentals-Part-1/link.js
+++ 01-Fundamentals-Part-1/link.js
@@ -1,8 +1,8 @@
 ////////////////////////////////////
 // Linking a JavaScript File
+
 // let js = "amazing";
 // if (js === "amazing") {
 //   alert("Javascript is FUN!");
 // }
 // console.log(45 + 55 + 20 - 30);
-
