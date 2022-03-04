38a3f198a67380b7b8fdd4b2b3ecd46b7af87ea3
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-04 10:14:06 +0530

    add file for let, const and var

---
+++ 01-Fundamentals-Part-1/let-const-var.js
@@ -0,0 +1,15 @@
+////////////////////////////////////
+// let, const and var
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Schmedtmann";
+console.log(lastName);
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -8,21 +8,6 @@
 
 
 ////////////////////////////////////
-// let, const and var
-// let age = 30;
-// age = 31;
-
-// const birthYear = 1991;
-// // birthYear = 1990;
-// // const job;
-
-// var job = "programmer";
-// job = "teacher";
-
-// lastName = "Schmedtmann";
-// console.log(lastName);
-
-////////////////////////////////////
 // Basic Operators
 // Math operators
 // const now = 2037;
