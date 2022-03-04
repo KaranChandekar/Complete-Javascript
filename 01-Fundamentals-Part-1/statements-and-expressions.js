6c2cf92852abdfc8116a61183a16b76f7ddb4907
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-04 13:23:15 +0530

    statements and expressions

---
+++ 01-Fundamentals-Part-1/statements-and-expressions.js
@@ -0,0 +1,12 @@
+////////////////////////////////////
+// Statements and Expressions
+3 + 4;
+1991;
+true && false && !false;
+
+if (23 > 10) {
+  const str = "23 is bigger";
+}
+
+const me = "Jonas";
+console.log(`I'm ${2037 - 1991} years old ${me}`);
