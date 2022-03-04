e0f169d87e8c52a88fcbebc2f7609b80dd05ac95
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-04 13:12:33 +0530

    truthy and falsy values

---
+++ 01-Fundamentals-Part-1/truthy-and-falsy-values.js
@@ -0,0 +1,23 @@
+////////////////////////////////////
+// Truthy and Falsy Values
+
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height = 0;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
