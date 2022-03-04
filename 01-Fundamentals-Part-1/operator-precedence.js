09b8b070cb98e4ca3d810fd3353907248550cec9
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-04 10:23:05 +0530

    operator precedence

---
+++ 01-Fundamentals-Part-1/operator-precedence.js
@@ -0,0 +1,15 @@
+////////////////////////////////////
+// Operator Precedence
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
