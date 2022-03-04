2961cfa243c993d0411c13ea9b061e4a2cc7d661
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-04 13:04:36 +0530

    if else statements

---
+++ 01-Fundamentals-Part-1/if-else.js
@@ -0,0 +1,21 @@
+////////////////////////////////////
+// Taking Decisions: if / else Statements
+
+const age = 15;
+
+if (age >= 18) {
+  console.log("Sarah can start driving license 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
