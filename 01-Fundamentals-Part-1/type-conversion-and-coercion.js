0d7e45bd548585c6f9a0bbb9b112346007186783
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-04 13:10:49 +0530

    type conversion and coercion

---
+++ 01-Fundamentals-Part-1/type-conversion-and-coercion.js
@@ -0,0 +1,21 @@
+////////////////////////////////////
+// Type Conversion and Coercion
+
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" / "2");
+
+let n = "1" + 1; // '11'
+n = n - 1;
+console.log(n);
