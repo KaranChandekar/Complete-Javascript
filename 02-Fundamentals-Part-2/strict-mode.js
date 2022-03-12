1a9dbc2b3337c12baf3fd4b92e16194a1a18cb9b
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-12 17:48:27 +0530

    activating strict mode

---
+++ 02-Fundamentals-Part-2/strict-mode.js
@@ -0,0 +1,13 @@
+////////////////////////////////////
+// Activating Strict Mode
+
+"use strict";
+
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest) hasDriversLicence = true;
+if (hasDriversLicence) console.log("I can drive :D");
+
+// const interface = "Audio";
+// const private = 543;
