be78b0fb90c4214bbd5a9c6a02608a37531bcd18
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-15 20:34:43 +0530

    reviewing functions

---
+++ 02-Fundamentals-Part-2/reviewing-functions.js
@@ -0,0 +1,24 @@
+///////////////////////////////////////
+// Reviewing Functions
+
+"use strict";
+
+const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+};
+
+const yearsUntilRetirement = function (birthYeah, firstName) {
+  const age = calcAge(birthYeah);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+};
+
+console.log(yearsUntilRetirement(2003, "Karan"));
+console.log(yearsUntilRetirement(2006, "Kalpit"));
