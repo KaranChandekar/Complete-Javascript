c80fe1b51fc73f63910c77cf50ccfd11766b0ab9
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-17 16:35:25 +0530

    modify arrays

--- 02-Fundamentals-Part-2/arrays.js
+++ 02-Fundamentals-Part-2/arrays.js
@@ -1,11 +1,11 @@
 ///////////////////////////////////////
 // Introduction to Arrays
 
-const friend1 = "Michael";
-const friend2 = "Steven";
-const friend3 = "Peter";
+const friend1 = "Vikrant";
+const friend2 = "Amit";
+const friend3 = "Dip";
 
-const friends = ["Michael", "Steven", "Peter"];
+const friends = ["Vikrant", "Amit", "Dip"];
 console.log(friends);
 
 const y = new Array(1991, 1984, 2008, 2020);
@@ -16,20 +16,20 @@ console.log(friends[2]);
 console.log(friends.length);
 console.log(friends[friends.length - 1]);
 
-friends[2] = "Jay";
+friends[2] = "Devid";
 console.log(friends);
 // friends = ['Bob', 'Alice']
 
-const firstName = "Jonas";
-const jonas = [firstName, "Schmedtmann", 2037 - 1991, "teacher", friends];
-console.log(jonas);
-console.log(jonas.length);
+const firstName = "Karan";
+const karan = [firstName, "Chandekar", 2022 - 2003, "student", friends];
+console.log(karan);
+console.log(karan.length);
 
 // Exercise
 const calcAge = function (birthYeah) {
-  return 2037 - birthYeah;
+  return 2022 - birthYeah;
 };
-const years = [1990, 1967, 2002, 2010, 2018];
+const years = [1990, 1967, 2003, 2010, 2015];
 
 const age1 = calcAge(years[0]);
 const age2 = calcAge(years[1]);
