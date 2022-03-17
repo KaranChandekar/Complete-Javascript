bf3bab4683c9b694ffb8fb24bf116c6026137e6b
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-17 16:59:48 +0530

    add elements

--- 02-Fundamentals-Part-2/basic-array-methods.js
+++ 02-Fundamentals-Part-2/basic-array-methods.js
@@ -2,3 +2,11 @@
 // Basic Array Operations (Methods)
 
 const friends = ["Vikrant", "Amit", "Dip"];
+
+// Add elements
+const newLength = friends.push("Pankaj");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("Amol");
+console.log(friends);
