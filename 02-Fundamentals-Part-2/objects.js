95d680ea676fa21a3423413f5ea1334152fcbdfe
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-18 13:28:35 +0530

    objects introduction

---
+++ 02-Fundamentals-Part-2/objects.js
@@ -0,0 +1,18 @@
+///////////////////////////////////////
+// Introduction to Objects
+
+const jonasArray = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+];
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
