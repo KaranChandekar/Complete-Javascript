f30b75c20caa2ead0dabd62d02edb38687a367ea
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-19 23:15:36 +0530

    dot vs bracket notation

---
+++ 02-Fundamentals-Part-2/dot-vs-bracket-notation.js
@@ -0,0 +1,14 @@
+///////////////////////////////////////
+// Dot vs. Bracket Notation
+
+const karan = {
+  firstName: "Karan",
+  lastName: "Chandekar",
+  age: 2022 - 2003,
+  job: "student",
+  friends: ["Vikrant", "Amit", "Dip"],
+};
+console.log(karan);
+
+console.log(karan.lastName);
+console.log(karan["lastName"]);
