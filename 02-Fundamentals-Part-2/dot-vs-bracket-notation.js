db7c485c5611a0faf0a623a4854ce60578bb358d
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-19 23:21:05 +0530

    using prompt in objects

--- 02-Fundamentals-Part-2/dot-vs-bracket-notation.js
+++ 02-Fundamentals-Part-2/dot-vs-bracket-notation.js
@@ -18,3 +18,19 @@ console.log(karan["first" + nameKey]);
 console.log(karan["last" + nameKey]);
 
 // console.log(karan.'last' + nameKey)
+
+const interestedIn = prompt(
+  "What do you want to know about Karan? Choose between firstName, lastName, age, job, and friends"
+);
+
+if (karan[interestedIn]) {
+  console.log(karan[interestedIn]);
+} else {
+  console.log(
+    "Wrong request! Choose between firstName, lastName, age, job, and friends"
+  );
+}
+
+karan.location = "Maharashtra";
+karan["twitter"] = "@karan_chandekar";
+console.log(karan);
