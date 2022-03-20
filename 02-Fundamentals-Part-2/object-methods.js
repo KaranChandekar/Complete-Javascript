7ed1dccbf340919b8913e242bd90c758b7aad19b
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-20 09:09:41 +0530

    function expression in objects

---
+++ 02-Fundamentals-Part-2/object-methods.js
@@ -0,0 +1,15 @@
+///////////////////////////////////////
+// Object Methods
+
+const karan = {
+  firstName: "Karan",
+  lastName: "Chandekar",
+  birthYeah: 2003,
+  occupation: "student",
+  friends: ["Vikrant", "Amit", "Dip"],
+  hasDriversLicense: true,
+
+  calcAge: function (birthYeah) {
+    return 2037 - birthYeah;
+  }
+};
