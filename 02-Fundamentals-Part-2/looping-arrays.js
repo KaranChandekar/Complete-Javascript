e39f6cfbea547f8ef97205fcdd629fd93f08ffb4
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-25 23:56:58 +0530

    looping array

---
+++ 02-Fundamentals-Part-2/looping-arrays.js
@@ -0,0 +1,18 @@
+///////////////////////////////////////
+// Looping Arrays, Breaking and Continuing
+
+const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+const types = [];
+
+// console.log(jonas[0])
+// console.log(jonas[1])
+// ...
+// console.log(jonas[4])
+// jonas[5] does NOT exist
