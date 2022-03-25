72a7cd86e52bb4929140b95526970cc70ae25c8d
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-25 23:58:50 +0530

    continue and break

--- 02-Fundamentals-Part-2/looping-arrays.js
+++ 02-Fundamentals-Part-2/looping-arrays.js
@@ -27,3 +27,19 @@ for (let i = 0; i < jonas.length; i++) {
 }
 
 console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+// continue and break
+console.log("--- ONLY STRINGS ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "string") continue;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
