1d16cca49ba9ec027268e418a5af0d1253c84048
Karan Chandekar <karanchandekar431@gmail.com>
2022-03-25 23:59:32 +0530

    break with number

--- 02-Fundamentals-Part-2/looping-arrays.js
+++ 02-Fundamentals-Part-2/looping-arrays.js
@@ -43,3 +43,10 @@ for (let i = 0; i < jonas.length; i++) {
 
   console.log(jonas[i], typeof jonas[i]);
 }
+
+console.log("--- BREAK WITH NUMBER ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === "number") break;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
