4f537e896bb50e020dcf1510faed02086080fd10
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-04 20:37:06 +0530

    another example of while loop

--- 02-Fundamentals-Part-2/while-loop.js
+++ 02-Fundamentals-Part-2/while-loop.js
@@ -3,3 +3,9 @@
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
 }
+
+let rep = 1;
+while (rep <= 10) {
+  // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
+  rep++;
+}
