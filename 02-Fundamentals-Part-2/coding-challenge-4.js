a056f32d87d073943b58303e77283d90aea17882
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-05 18:56:29 +0530

    adding arrays

--- 02-Fundamentals-Part-2/coding-challenge-4.js
+++ 02-Fundamentals-Part-2/coding-challenge-4.js
@@ -25,3 +25,6 @@ GOOD LUCK 😀
 const calcTip = function (bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
