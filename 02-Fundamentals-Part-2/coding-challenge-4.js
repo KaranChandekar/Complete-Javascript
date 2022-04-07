a612020034895df80a784daacb658a84868f82e3
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-07 23:00:38 +0530

    add function

--- 02-Fundamentals-Part-2/coding-challenge-4.js
+++ 02-Fundamentals-Part-2/coding-challenge-4.js
@@ -35,3 +35,15 @@ for (let i = 0; i < bills.length; i++) {
   totals.push(tip + bills[i]);
 }
 console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
