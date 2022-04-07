7156b3cd18c0c2154aacde08a9a9f110071d68e5
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-07 22:59:59 +0530

    add for loop

--- 02-Fundamentals-Part-2/coding-challenge-4.js
+++ 02-Fundamentals-Part-2/coding-challenge-4.js
@@ -28,3 +28,10 @@ const calcTip = function (bill) {
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
 const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
