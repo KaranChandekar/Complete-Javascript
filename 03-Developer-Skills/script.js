63042f42d5c18b732a25c9f3e419be88afdc7f6d
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-12 23:31:38 +0530

    add temperature array

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -6,6 +6,8 @@
 // PROBLEM 1:
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
 // 1) Understanding the problem
 // - What is temp amplitude? Answer: difference between highest and lowest temp
 // - How to compute max and min temperatures?
