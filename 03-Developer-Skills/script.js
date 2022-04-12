c560f60940bce683b6f2869ee8a3ff3eb13d89ed
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-12 23:33:36 +0530

    problem 2

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -35,3 +35,12 @@ const calcTempAmplitude = function (temps) {
 };
 const amplitude = calcTempAmplitude(temperatures);
 console.log(amplitude);
+
+// PROBLEM 2:
+// Function should now receive 2 arrays of temps
+
+// 1) Understanding the problem
+// - With 2 arrays, should we implement functionality twice? NO! Just merge two arrays
+
+// 2) Breaking up into sub-problems
+// - Merge 2 arrays
