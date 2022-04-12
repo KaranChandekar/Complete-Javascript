8fe27dfdbbf4ba0a9490c6934149fb1279632793
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-12 23:34:15 +0530

    2nd solution

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -44,3 +44,23 @@ console.log(amplitude);
 
 // 2) Breaking up into sub-problems
 // - Merge 2 arrays
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
