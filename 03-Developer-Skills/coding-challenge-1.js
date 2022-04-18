9ddd27ba6c24364546d133cf1a53eedede0fc901
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-18 20:00:04 +0530

    coding challenge solution

--- 03-Developer-Skills/coding-challenge-1.js
+++ 03-Developer-Skills/coding-challenge-1.js
@@ -24,3 +24,17 @@ TEST DATA 2: [12, 5, -5, 0, 4]
 // - Strings needs to contain day (index + 1)
 // - Add ... between elements and start and end of string
 // - Log string to console
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
+
+const printForecast = function (arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  console.log("..." + str);
+};
+printForecast(data1);
