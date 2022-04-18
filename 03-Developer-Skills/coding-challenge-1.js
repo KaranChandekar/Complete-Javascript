5b50150a48d7dcdf009e013a0386c95a43ab232c
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-18 19:59:21 +0530

    steps for challenge 1

--- 03-Developer-Skills/coding-challenge-1.js
+++ 03-Developer-Skills/coding-challenge-1.js
@@ -13,3 +13,14 @@ Use the problem-solving framework: Understand the problem and break it up into s
 TEST DATA 1: [17, 21, 23]
 TEST DATA 2: [12, 5, -5, 0, 4]
 */
+
+// 1) Understanding the problem
+// - Array transformed to string, separated by ...
+// - What is the X days? Answer: index + 1
+
+// 2) Breaking up into sub-problems
+// - Transform array into string
+// - Transform each element to string with ºC
+// - Strings needs to contain day (index + 1)
+// - Add ... between elements and start and end of string
+// - Log string to console
