4496a4d41ee29ff6a1a743bd78cb7d2091e526af
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-18 19:51:07 +0530

    using a debugger

--- 03-Developer-Skills/debug.js
+++ 03-Developer-Skills/debug.js
@@ -22,3 +22,25 @@ const measureKelvin = function () {
 };
 // A) IDENTIFY
 console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTIFY
+console.log(amplitudeBug);
