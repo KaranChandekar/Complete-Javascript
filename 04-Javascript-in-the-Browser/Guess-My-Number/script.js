0574770205f7b894f25f31fa464dc26069ff0a01
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-10 23:03:45 +0530

    add random number

--- 04-Javascript-in-the-Browser/Guess-My-Number/script.js
+++ 04-Javascript-in-the-Browser/Guess-My-Number/script.js
@@ -4,6 +4,9 @@ const checkBtn = document.querySelector('.check');
 const guessInput = document.querySelector('.guess');
 const message = document.querySelector('.message');
 
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 checkBtn.addEventListener('click', function () {
   const guess = Number(guessInput.value);
   console.log(guess, typeof guess);
