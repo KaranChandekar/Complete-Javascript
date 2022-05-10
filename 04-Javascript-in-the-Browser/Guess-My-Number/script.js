0dbc5bc7f5cc13e50ec4ccd3a7054badc2196eff
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-10 19:56:07 +0530

    add click event

--- 04-Javascript-in-the-Browser/Guess-My-Number/script.js
+++ 04-Javascript-in-the-Browser/Guess-My-Number/script.js
@@ -3,3 +3,8 @@
 const checkBtn = document.querySelector('.check');
 const guessInput = document.querySelector('.guess');
 const message = document.querySelector('.message');
+
+checkBtn.addEventListener('click', function () {
+  const guess = Number(guessInput.value);
+  console.log(guess, typeof guess);
+});
