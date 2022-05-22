1517434b47ca0d61fc216f6ec568ed3c677a6f69
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-22 18:53:45 +0530

    objects vs primitives

---
+++ 05-Behind-the-Scenes/objects-vs-primitives.js
@@ -0,0 +1,18 @@
+'use strict';
+
+///////////////////////////////////////
+// Objects vs. primitives
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log('Friend:', friend);
+console.log('Me', me);
