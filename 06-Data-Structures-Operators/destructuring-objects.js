1e4ce0e500b9108249b31f1c84db13acc078e12c
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-23 20:03:27 +0530

    mutating variables

--- 06-Data-Structures-Operators/destructuring-objects.js
+++ 06-Data-Structures-Operators/destructuring-objects.js
@@ -27,3 +27,10 @@ console.log(restaurantName, hours, tags);
 // Default values
 const { menu = [], starterMenu: starters = [] } = restaurant;
 console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
