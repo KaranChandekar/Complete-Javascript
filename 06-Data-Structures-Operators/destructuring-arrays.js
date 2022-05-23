b718ce07bbd3519e449b833b6f9a1c8a5cc13219
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-23 18:57:46 +0530

    destructuring arrays

---
+++ 06-Data-Structures-Operators/destructuring-arrays.js
@@ -0,0 +1,15 @@
+'use strict';
+
+///////////////////////////////////////
+// Destructuring Arrays
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
