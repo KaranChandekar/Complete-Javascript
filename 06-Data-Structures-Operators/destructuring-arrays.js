6dd2cf7291f55776da4e13dca5360a6ced1cfa04
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-23 18:59:42 +0530

    nested destructuring

--- 06-Data-Structures-Operators/destructuring-arrays.js
+++ 06-Data-Structures-Operators/destructuring-arrays.js
@@ -26,3 +26,9 @@ console.log(main, secondary);
 // Receive 2 return values from a function
 const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
+
+// Nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
