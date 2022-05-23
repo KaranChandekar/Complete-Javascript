59b86970e69aba0a5e61463e8649c6371ff9d6d0
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-23 18:59:58 +0530

    default values

--- 06-Data-Structures-Operators/destructuring-arrays.js
+++ 06-Data-Structures-Operators/destructuring-arrays.js
@@ -32,3 +32,7 @@ const nested = [2, 4, [5, 6]];
 // const [i, , j] = nested;
 const [i, , [j, k]] = nested;
 console.log(i, j, k);
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
