8aa2651af243df1a9a957ecdc4d5a98649e96ce4
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-24 21:16:41 +0530

    spread operator

---
+++ 06-Data-Structures-Operators/spread-operator.js
@@ -0,0 +1,17 @@
+'use strict';
+
+///////////////////////////////////////
+// The Spread Operator (...)
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
