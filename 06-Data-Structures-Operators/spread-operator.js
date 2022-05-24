66dcce708685f23264b2336ad53c78f10b6873e8
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-24 21:17:29 +0530

    join 2 arrays

--- 06-Data-Structures-Operators/spread-operator.js
+++ 06-Data-Structures-Operators/spread-operator.js
@@ -18,3 +18,7 @@ console.log(newMenu);
 
 // Copy array
 const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
