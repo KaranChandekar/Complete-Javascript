44a2a61b8fdfe49fe2b40f6bc056241147650173
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-25 17:57:34 +0530

    functions

--- 06-Data-Structures-Operators/rest-pattern-and-parameters.js
+++ 06-Data-Structures-Operators/rest-pattern-and-parameters.js
@@ -20,3 +20,20 @@ console.log(pizza, risotto, otherFood);
 // Objects
 const { sat, ...weekdays } = restaurant.openingHours;
 console.log(weekdays);
+
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
