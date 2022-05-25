acef1aa2bf01646de6dae3651f5324b0142f0f99
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-25 17:57:19 +0530

    rest pattern and parameters

---
+++ 06-Data-Structures-Operators/rest-pattern-and-parameters.js
@@ -0,0 +1,22 @@
+'use strict';
+
+///////////////////////////////////////
+// Rest Pattern and Parameters
+// 1) Destructuring
+
+// SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
