f02ce82359a1932edc6886a2b2885611be214200
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-27 19:56:19 +0530

    logical assignment operator

---
+++ 06-Data-Structures-Operators/logical-assign-operator.js
@@ -0,0 +1,14 @@
+'use strict';
+
+///////////////////////////////////////
+// Logical Assignment Operators
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
