f82e5bd6d88ab5ced48dd6ecad0d4a15dd74db7e
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-27 19:52:11 +0530

    nullish: null and undefined

--- 06-Data-Structures-Operators/nullish.js
+++ 06-Data-Structures-Operators/nullish.js
@@ -28,3 +28,7 @@ const restaurant = {
 restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
 console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
