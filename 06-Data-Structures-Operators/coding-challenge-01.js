4d28d9109c061039a9761346013840d9646ee1f8
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-01 19:16:35 +0530

    solved step 5

--- 06-Data-Structures-Operators/coding-challenge-01.js
+++ 06-Data-Structures-Operators/coding-challenge-01.js
@@ -75,3 +75,9 @@ console.log(allPlayers);
 // 4.
 const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
 console.log(players1Final);
+
+// 5.
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
