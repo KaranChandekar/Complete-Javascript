3fde1cd6a3b54b6a43c981d2281a46a1d8b6730d
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-06 23:31:27 +0530

    key, value

--- 06-Data-Structures-Operators/looping-object.js
+++ 06-Data-Structures-Operators/looping-object.js
@@ -42,3 +42,8 @@ console.log(values);
 // Entire object
 const entries = Object.entries(openingHours);
 console.log(entries);
+
+// [key, value]
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
