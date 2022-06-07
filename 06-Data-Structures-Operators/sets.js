fda3bd26f8f1109ff9eb9e8f3e961a2883dee78e
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-07 21:53:36 +0530

    set.add

--- 06-Data-Structures-Operators/sets.js
+++ 06-Data-Structures-Operators/sets.js
@@ -16,3 +16,5 @@ console.log(new Set('Jonas'));
 console.log(ordersSet.size);
 console.log(ordersSet.has('Pizza'));
 console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
