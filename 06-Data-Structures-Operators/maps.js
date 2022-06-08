7adc743247223628b17db92fc14d338674b3206d
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-08 22:39:13 +0530

    .delete

--- 06-Data-Structures-Operators/maps.js
+++ 06-Data-Structures-Operators/maps.js
@@ -19,3 +19,7 @@ console.log(rest.get(1));
 
 const time = 8;
 console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
