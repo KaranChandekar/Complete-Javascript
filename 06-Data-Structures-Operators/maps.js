c805a0bc36796c8499a342f7419fb0cfd240b417
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-08 22:38:00 +0530

    .set

--- 06-Data-Structures-Operators/maps.js
+++ 06-Data-Structures-Operators/maps.js
@@ -5,3 +5,14 @@ const rest = new Map();
 rest.set('name', 'Classico Italiano');
 rest.set(1, 'Firenze, Italy');
 console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
