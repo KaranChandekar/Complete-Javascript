ce5f91670829e1716295a7e71fad541a731a22fc
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-08 22:37:13 +0530

    maps fundamentals

---
+++ 06-Data-Structures-Operators/maps.js
@@ -0,0 +1,7 @@
+'use strict';
+///////////////////////////////////////
+// Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
