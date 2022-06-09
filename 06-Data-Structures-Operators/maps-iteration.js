dade836120efdaba6082df3f6c3d30ce9a8541f3
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-09 20:20:18 +0530

    quiz app

--- 06-Data-Structures-Operators/maps-iteration.js
+++ 06-Data-Structures-Operators/maps-iteration.js
@@ -16,3 +16,14 @@ console.log(question);
 console.log(Object.entries(openingHours));
 const hoursMap = new Map(Object.entries(openingHours));
 console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
