c537865bb279e499df3cec74fb442f6b794891a2
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-09 20:19:46 +0530

    convert object to map

---
+++ 06-Data-Structures-Operators/maps-iteration.js
@@ -0,0 +1,18 @@
+'use strict';
+///////////////////////////////////////
+// Maps: Iteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
