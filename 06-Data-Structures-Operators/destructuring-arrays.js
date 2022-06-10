bb196b862f87cad5699b262fb57fe7c993778674
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-10 21:53:03 +0530

    added restaurant info

--- 06-Data-Structures-Operators/destructuring-arrays.js
+++ 06-Data-Structures-Operators/destructuring-arrays.js
@@ -1,4 +1,11 @@
 'use strict';
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+};
 
 ///////////////////////////////////////
 // Destructuring Arrays
