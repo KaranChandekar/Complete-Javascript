f93f4e0c658d5446268e89ad44d9174f070b3f49
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-11 23:46:03 +0530

    updated spread operator

--- 06-Data-Structures-Operators/spread-operator.js
+++ 06-Data-Structures-Operators/spread-operator.js
@@ -3,6 +3,50 @@
 ///////////////////////////////////////
 // The Spread Operator (...)
 
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delived to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+};
+
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
@@ -25,7 +69,7 @@ console.log(menu);
 
 // Iterables: arrays, strings, maps, sets. NOT objects
 const str = 'Karan';
-const letters = [...str, ' ', 'S.'];
+const letters = [...str, ' ', 'C.'];
 console.log(letters);
 console.log(...str);
 // console.log(`${...str} Chandekar`);
