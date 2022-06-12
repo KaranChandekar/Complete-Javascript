606bbed20c552ff0569be522e930658fa3b75a7c
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-12 21:32:53 +0530

    third enhancement

--- 06-Data-Structures-Operators/enhanced-object-literals.js
+++ 06-Data-Structures-Operators/enhanced-object-literals.js
@@ -2,16 +2,18 @@
 ///////////////////////////////////////
 // Enhanced Object Literals
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
 const openingHours = {
-  thu: {
+  [weekdays[4]]: {
     open: 12,
     close: 22,
   },
-  fri: {
+  [weekdays[5]]: {
     open: 11,
     close: 23,
   },
-  sat: {
+  [`day ${4 + 2}`]: {
     open: 0, // Open 24 hours
     close: 24,
   },
