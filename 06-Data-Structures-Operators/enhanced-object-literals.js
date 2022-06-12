70f94b3fcdb205173b43856d7f36ae35192daace
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-12 21:22:27 +0530

    second enhancement

--- 06-Data-Structures-Operators/enhanced-object-literals.js
+++ 06-Data-Structures-Operators/enhanced-object-literals.js
@@ -27,22 +27,17 @@ const restaurant = {
   // ES6 enhanced object literals
   openingHours,
 
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delived to ${address} at ${time}`
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
