9083fb5a5b1712a327d12c4034285ea5e53329bc
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-16 20:01:27 +0530

    created flighBooking function

--- 07-Functions/default-parameters.js
+++ 07-Functions/default-parameters.js
@@ -2,3 +2,14 @@
 
 ///////////////////////////////////////
 // Default Parameters
+
+const flightBooking = function (flightNum, numPassengers, price) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+};
+
+flightBooking();
