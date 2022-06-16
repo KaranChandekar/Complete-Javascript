6142e51df19affaa216bc6b20285f2f0d2854367
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-16 21:31:23 +0530

    added checkIn function

--- 07-Functions/values-vs-reference.js
+++ 07-Functions/values-vs-reference.js
@@ -8,3 +8,21 @@ const karan = {
   name: 'Karan Chandekar',
   passport: 267395148097,
 };
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LKH999';
+  passenger.name = 'Mr. ' + passenger.name;
+  if (passenger.passport === 267395148097) {
+    alert('Checked In');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, karan);
+console.log(flight);
+console.log(karan);
+
+// is the same as doing...
+const flightNum = flight;
+const passenger = karan;
