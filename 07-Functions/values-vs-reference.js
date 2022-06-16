ed5c73e5fc21385b73d5a7aaaa68ae86660ef3d1
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-16 21:09:02 +0530

    created flight and passenger

---
+++ 07-Functions/values-vs-reference.js
@@ -0,0 +1,10 @@
+'use strict';
+
+///////////////////////////////////////
+// How Passing Arguments Works: Values vs. Reference
+
+const flight = 'LKH234';
+const karan = {
+  name: 'Karan Chandekar',
+  passport: 267395148097,
+};
