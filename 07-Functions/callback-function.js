5943e4aa4d4aff9f99a58ae4cfc5a1c43ab63d1c
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-17 15:12:42 +0530

    created two functions

--- 07-Functions/callback-function.js
+++ 07-Functions/callback-function.js
@@ -1,3 +1,15 @@
 'use strict';
 ///////////////////////////////////////
 // Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+console.log(oneWord('Karan Chandekar'));
+console.log(upperFirstWord('Karan Chandekar'));
