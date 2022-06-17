e1867b9284c8bc28275e5e5959003eab805b3260
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-17 19:29:48 +0530

    greet function returned greeterHey function

--- 07-Functions/functions-returning-functions.js
+++ 07-Functions/functions-returning-functions.js
@@ -1,3 +1,15 @@
 'use strict';
 ///////////////////////////////////////
 // Functions Returning Functions
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+const greeterHey = greet('Hey');
+greeterHey('Karan!');
+greeterHey('Steve!');
+
+// is same as...
+greet('Hello')('Karan');
