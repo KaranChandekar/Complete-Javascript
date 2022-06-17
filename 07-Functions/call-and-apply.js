e22c952479efa412e6e9ec0e817aaef1541eeca4
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-17 21:49:37 +0530

    created function inside object

--- 07-Functions/call-and-apply.js
+++ 07-Functions/call-and-apply.js
@@ -1,3 +1,16 @@
 'use strict';
 ///////////////////////////////////////
 // The call and apply Methods
+const indigo = {
+  airline: 'Indigo',
+  iataCode: '6E',
+  bookings: [],
+  // book: function(){}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+  },
+};
+indigo.book(123, 'Karan Chandekar');
+indigo.book(456, 'John Wick');
