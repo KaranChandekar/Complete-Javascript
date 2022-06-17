5977f777f334f94c87129a82c4dd46ad4ca3b228
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-17 15:33:15 +0530

    accepted callback functions by function

--- 07-Functions/callback-function.js
+++ 07-Functions/callback-function.js
@@ -13,3 +13,12 @@ const upperFirstWord = function (str) {
 
 console.log(oneWord('Karan Chandekar'));
 console.log(upperFirstWord('Karan Chandekar'));
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
