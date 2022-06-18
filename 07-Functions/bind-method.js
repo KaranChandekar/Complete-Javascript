5011984939727e8bdc53fc2c99bf775fa8a77e91
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-18 18:53:08 +0530

    partial application

--- 07-Functions/bind-method.js
+++ 07-Functions/bind-method.js
@@ -42,3 +42,12 @@ indigo.buyPlane = function () {
 document
   .querySelector('.buy')
   .addEventListener('click', indigo.buyPlane.bind(indigo));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// const addVAT = value => value + value * 0.23;
+console.log(addVAT(100));
+console.log(addVAT(23));
