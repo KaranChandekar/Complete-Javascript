21e86227395e5638046b63a464532604533806de
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-20 18:19:52 +0530

    added poll object

--- 07-Functions/coding-challenge-01.js
+++ 07-Functions/coding-challenge-01.js
@@ -31,3 +31,9 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 
 GOOD LUCK 😀
 */
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+};
