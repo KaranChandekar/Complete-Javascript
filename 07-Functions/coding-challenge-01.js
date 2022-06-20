360d491de3a1367bd244d8616398b48effd6aafd
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-20 18:48:17 +0530

    solved step 1 to get answer

--- 07-Functions/coding-challenge-01.js
+++ 07-Functions/coding-challenge-01.js
@@ -36,4 +36,15 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section 😃
   answers: new Array(4).fill(0),
+  // 1.
+  registerNewAnswer() {
+    // Get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+  },
 };
+poll.registerNewAnswer();
