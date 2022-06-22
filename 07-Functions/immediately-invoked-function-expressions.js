5d14d8dbc8433314ed78496369642aa1945b2caa
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-22 22:42:12 +0530

    another same thing with notPrivate variable

--- 07-Functions/immediately-invoked-function-expressions.js
+++ 07-Functions/immediately-invoked-function-expressions.js
@@ -15,3 +15,10 @@ runOnce();
 // console.log(isPrivate);
 
 (() => console.log('This will ALSO never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+// console.log(isPrivate);
+console.log(notPrivate);
