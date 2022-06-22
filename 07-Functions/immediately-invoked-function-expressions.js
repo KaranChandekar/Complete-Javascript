34e962cb2d9c7d880eff945ecd1e4353018521b2
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-22 22:39:08 +0530

    created function runOnce

--- 07-Functions/immediately-invoked-function-expressions.js
+++ 07-Functions/immediately-invoked-function-expressions.js
@@ -1,3 +1,7 @@
 'use strict';
 ///////////////////////////////////////
 // Immediately Invoked Function Expressions (IIFE)
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
