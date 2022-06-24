9a4acaa3a1cd1fc40997eef024521a48c6164730
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-24 13:59:44 +0530

    closures in practice

--- 07-Functions/closures.js
+++ 07-Functions/closures.js
@@ -1,3 +1,17 @@
 'use strict';
 ///////////////////////////////////////
 // Closures
+const secureBooking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+booker();
+booker();
+booker();
+
+console.dir(booker);
