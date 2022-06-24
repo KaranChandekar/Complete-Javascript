27a50041667e885c3d4c8c0e32d7cf0276643bd1
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-24 21:10:05 +0530

    Slice method in array

--- 08-Arrays-Bankist/simple-array-methods.js
+++ 08-Arrays-Bankist/simple-array-methods.js
@@ -1,3 +1,13 @@
 'use strict';
 /////////////////////////////////////////////////
 // Simple Array Methods
+const arr = ['a', 'b', 'c', 'd', 'e'];
+
+// Slice
+console.log(arr.slice(2));
+console.log(arr.slice(1, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(2, -1));
+console.log(arr.slice());
+console.log([...arr]);
