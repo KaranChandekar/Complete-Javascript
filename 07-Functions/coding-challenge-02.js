857d43cdbcdaa28a415b7c81670819217900b2fd
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-24 16:34:53 +0530

    solved coding challenge 2

--- 07-Functions/coding-challenge-02.js
+++ 07-Functions/coding-challenge-02.js
@@ -15,4 +15,8 @@ GOOD LUCK 😀
 (function () {
   const header = document.querySelector('h1');
   header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
 })();
