cac4b2c81ae35ec3da80ec636c281caf0d34b01d
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-24 14:27:26 +0530

    1st example of closure

--- 07-Functions/closure-examples.js
+++ 07-Functions/closure-examples.js
@@ -1,3 +1,27 @@
 'use strict';
 ///////////////////////////////////////
 // More Closure Examples
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 666;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+f();
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
