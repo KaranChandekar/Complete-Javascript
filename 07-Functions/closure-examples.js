045b9fc284e5e623704c6b3010c7a7e941ac1ae0
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-24 14:51:09 +0530

    2nd example of closure

--- 07-Functions/closure-examples.js
+++ 07-Functions/closure-examples.js
@@ -25,3 +25,15 @@ f();
 h();
 f();
 console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(() => {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+const perGroup = 1000;
+boardPassengers(120, 3);
