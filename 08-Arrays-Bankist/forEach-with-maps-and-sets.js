ec7993a71c2dbcb60989a0a8599542251f1092aa
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-25 10:49:24 +0530

    forEach with Maps and Sets

---
+++ 08-Arrays-Bankist/forEach-with-maps-and-sets.js
@@ -0,0 +1,8 @@
+'use strict';
+/////////////////////////////////////////////////
+// FOREACH with Maps and Sets
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
