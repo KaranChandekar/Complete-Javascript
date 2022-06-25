e5395fa254f28f6d884639d3f31eccd85113d329
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-26 00:40:57 +0530

    solved step 1 in coding challenge 2

--- 08-Arrays-Bankist/coding-challenge-02.js
+++ 08-Arrays-Bankist/coding-challenge-02.js
@@ -17,3 +17,10 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
 */
+
+const calcAverageHumanAge = function (ages) {
+  // 1.
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  console.log(humanAges);
+};
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
