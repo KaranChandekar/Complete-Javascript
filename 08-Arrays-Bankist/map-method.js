6de81e71085700a38a968b32d36c7fc4668a9716
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-25 18:51:20 +0530

    map method in practice

--- 08-Arrays-Bankist/map-method.js
+++ 08-Arrays-Bankist/map-method.js
@@ -2,3 +2,25 @@
 ///////////////////////////////////////
 // The map Method
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+
+// same with arrow function
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+console.log(movementsUSD);
+console.log(movements);
+
+// same with for-of loop
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${mov}`
+);
+console.log(movementsDescriptions);
