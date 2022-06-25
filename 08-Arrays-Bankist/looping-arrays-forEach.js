adfeeda84be3376abaea7e9042c9bfc7f47651e4
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-25 10:20:56 +0530

    simple loop of array

--- 08-Arrays-Bankist/looping-arrays-forEach.js
+++ 08-Arrays-Bankist/looping-arrays-forEach.js
@@ -2,3 +2,11 @@
 /////////////////////////////////////////////////
 // Looping Arrays - forEach
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
