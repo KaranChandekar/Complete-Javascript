c42a9d6e0272e59cb8a6c5e0e0767f49fe3a14e1
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-25 10:39:44 +0530

    looping array with for-of

--- 08-Arrays-Bankist/looping-arrays-forEach.js
+++ 08-Arrays-Bankist/looping-arrays-forEach.js
@@ -3,19 +3,19 @@
 // Looping Arrays - forEach
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
   if (movement > 0) {
-    console.log(`You deposited ${movement}`);
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
   } else {
-    console.log(`You withdrew ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
   }
 }
 
 console.log('--- FOREACH ---');
-movements.forEach(movement => {
-  if (movement > 0) {
-    console.log(`You deposited ${movement}`);
+movements.forEach((mov, i) => {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
   } else {
-    console.log(`You withdrew ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
   }
 });
