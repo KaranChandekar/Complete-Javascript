13445d841dd70c86014d77458935ba585af2c3b7
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-26 00:41:47 +0530

    solved step 2 in coding challenge

--- 08-Arrays-Bankist/coding-challenge-02.js
+++ 08-Arrays-Bankist/coding-challenge-02.js
@@ -22,5 +22,9 @@ const calcAverageHumanAge = function (ages) {
   // 1.
   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
   console.log(humanAges);
+
+  // 2.
+  const adults = humanAges.filter(age => age >= 18);
+  console.log(adults);
 };
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
