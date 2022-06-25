cb0ab51c19a42e4dd836e9032f003d63f20f8a7b
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-26 00:43:03 +0530

    solved step 4 in coding challenge 2

--- 08-Arrays-Bankist/coding-challenge-02.js
+++ 08-Arrays-Bankist/coding-challenge-02.js
@@ -36,4 +36,7 @@ const calcAverageHumanAge = function (ages) {
   );
   return average;
 };
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// 4.
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
