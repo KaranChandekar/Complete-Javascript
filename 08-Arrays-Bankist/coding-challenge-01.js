b00ef909752e9859570817bdde02047bd6aa2725
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-25 15:30:23 +0530

    solved step 4

--- 08-Arrays-Bankist/coding-challenge-01.js
+++ 08-Arrays-Bankist/coding-challenge-01.js
@@ -39,4 +39,7 @@ const checkDogs = (dogsJulia, dogsKate) => {
     console.log(age);
   });
 };
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+// 4.
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
