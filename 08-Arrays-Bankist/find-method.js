2e6ff95485e73e05cfa59061561b77d62c588ead
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-26 23:13:54 +0530

    more example of find method

--- 08-Arrays-Bankist/find-method.js
+++ 08-Arrays-Bankist/find-method.js
@@ -37,3 +37,7 @@ console.log(movements);
 
 const firstWithdrawal = movements.find(mov => mov < 0);
 console.log(firstWithdrawal);
+
+console.log(accounts);
+const account = accounts.find(acc => (acc.owner = 'Steven Thomas Williams'));
+console.log(account);
