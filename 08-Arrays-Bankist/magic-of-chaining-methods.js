244e25df2b0c8ddf9ae3cd4deb0cb5b819d8d850
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-26 13:16:34 +0530

    way to debug array methods pipeline

--- 08-Arrays-Bankist/magic-of-chaining-methods.js
+++ 08-Arrays-Bankist/magic-of-chaining-methods.js
@@ -7,6 +7,10 @@ const eurToUsd = 1.1;
 // PIPELINE
 const totalDepositsUSD = movements
   .filter(mov => mov > 0)
-  .map(mov => mov * eurToUsd)
+  .map((mov, i, arr) => {
+    console.log(arr);
+    return mov * eurToUsd;
+  })
+  //   .map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
