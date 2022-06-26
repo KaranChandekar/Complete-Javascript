436ecd7b5c434dd97474e53744703eb9ee097c04
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-26 14:28:07 +0530

    coding challenge 03

---
+++ 08-Arrays-Bankist/coding-challenge-03.js
@@ -0,0 +1,12 @@
+'use strict'
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
