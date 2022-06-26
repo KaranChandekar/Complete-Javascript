ea239423fbb8b5193df4bacf2d34d77108c5b3c3
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-26 14:35:03 +0530

    solved coding challenge 03

--- 08-Arrays-Bankist/coding-challenge-03.js
+++ 08-Arrays-Bankist/coding-challenge-03.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 ///////////////////////////////////////
 // Coding Challenge #3
 
@@ -10,3 +10,12 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
 */
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
