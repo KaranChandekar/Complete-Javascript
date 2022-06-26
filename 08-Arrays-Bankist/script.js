15ec7d998c049acf36d3f3028042c706963edd27
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-26 14:25:09 +0530

    display interest in Bankist app

--- 08-Arrays-Bankist/script.js
+++ 08-Arrays-Bankist/script.js
@@ -93,6 +93,13 @@ const calcDisplaySummary = function (movements) {
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+
+  const interest = movements
+    .filter(deposit => deposit > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter(int => int >= 1)
+    .reduce((acc, deposit) => acc + deposit, 0);
+  labelSumInterest.textContent = `${interest}€`;
 };
 calcDisplaySummary(account1.movements);
 
