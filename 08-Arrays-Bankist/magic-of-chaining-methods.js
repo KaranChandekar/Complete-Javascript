25973a342110feedfc761d7ad5854e7f7e35c680
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-26 13:12:30 +0530

    pipeline of array methods

--- 08-Arrays-Bankist/magic-of-chaining-methods.js
+++ 08-Arrays-Bankist/magic-of-chaining-methods.js
@@ -2,3 +2,11 @@
 ///////////////////////////////////////
 // The magic of chaining methods
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
