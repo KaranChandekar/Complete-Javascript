e0407874d0c836abc048a4605ae554121509bebc
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-28 00:26:24 +0530

    every method in array js

--- 08-Arrays-Bankist/some-and-every.js
+++ 08-Arrays-Bankist/some-and-every.js
@@ -1,12 +1,30 @@
 'use strict';
 ///////////////////////////////////////
 // some and every
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 console.log(movements);
 // EQUALITY
 console.log(movements.includes(-650));
 
-// CONDITION
+// SOME: Condition
 const anyDeposits = movements.some(mov => mov > 500);
 console.log(anyDeposits);
+
+// EVERY
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
