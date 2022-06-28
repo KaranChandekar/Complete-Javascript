f1d3fe9f3947f1648de71fbeea99e29f96060f3b
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-28 21:59:53 +0530

    sorting strings

--- 08-Arrays-Bankist/sorting-arrays.js
+++ 08-Arrays-Bankist/sorting-arrays.js
@@ -1,3 +1,8 @@
 'use strict';
 ///////////////////////////////////////
 // Sorting Arrays
+
+// Strings
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+console.log(owners);
