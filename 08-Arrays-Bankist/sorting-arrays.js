0f22ad5b9226a7c121cf67bf961be792d457ded2
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-28 22:00:58 +0530

    sorting numbers

--- 08-Arrays-Bankist/sorting-arrays.js
+++ 08-Arrays-Bankist/sorting-arrays.js
@@ -6,3 +6,25 @@
 const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
 console.log(owners.sort());
 console.log(owners);
+
+// Numbers
+console.log(movements);
+
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+
+// Ascending
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (a < b) return -1;
+// });
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+// Descending
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (a < b) return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements);
