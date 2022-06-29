9a2a9f0da8f20617114cc73bdedc1ec03fe50571
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-29 21:02:07 +0530

    fill method - arrays

--- 08-Arrays-Bankist/more-array-methods.js
+++ 08-Arrays-Bankist/more-array-methods.js
@@ -3,3 +3,14 @@
 // More Ways of Creating and Filling Arrays
 const arr = [1, 2, 3, 4, 5, 6, 7];
 console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Emprty arrays + fill method
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+x.fill(1, 3, 5);
+// x.fill(1);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
