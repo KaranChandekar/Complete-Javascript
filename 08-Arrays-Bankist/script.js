d89e1187ec1330ce69a4481cf908bde5d9c5bafc
Karan Chandekar <karanchandekar431@gmail.com>
2022-06-30 23:25:57 +0530

    exercise 2

--- 08-Arrays-Bankist/script.js
+++ 08-Arrays-Bankist/script.js
@@ -231,3 +231,8 @@ const bankDepositSum = accounts
   .filter(acc => acc > 0)
   .reduce((sum, cur) => sum + cur, 0);
 console.log(bankDepositSum);
+
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov >= 1000).length;
+console.log(numDeposits1000);
