72f76aa0692b34db691e8475bde090fbfdde4afc
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-01 21:41:05 +0530

    solved step 4

--- 08-Arrays-Bankist/coding-challenge-04.js
+++ 08-Arrays-Bankist/coding-challenge-04.js
@@ -59,3 +59,7 @@ const ownersEatTooLittle = dogs
   .filter(dog => dog.curFood < dog.recFood)
   .flatMap(dog => dog.owners);
 console.log(ownersEatTooLittle);
+
+// 4.
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too littel!`);
