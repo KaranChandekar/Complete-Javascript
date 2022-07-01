18f3553b61e832af485e35fb8bb97f9f1d64a2e6
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-01 21:10:43 +0530

    solved step 1

--- 08-Arrays-Bankist/coding-challenge-04.js
+++ 08-Arrays-Bankist/coding-challenge-04.js
@@ -35,3 +35,7 @@ const dogs = [
   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
   { weight: 32, curFood: 340, owners: ['Michael'] },
 ];
+
+// 1.
+dogs.forEach(dog => (dog.recFood = Math.floor(dog.weight ** 0.75 * 28)));
+console.log(dogs);
