671b07b3b1e0679248bf6c88e38fd7c2a315fc93
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-01 21:34:58 +0530

    solved step 3

--- 08-Arrays-Bankist/coding-challenge-04.js
+++ 08-Arrays-Bankist/coding-challenge-04.js
@@ -48,3 +48,14 @@ console.log(
     dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
   }`
 );
+
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recFood)
+  .flatMap(dog => dog.owners);
+//   .flat();
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooLittle);
