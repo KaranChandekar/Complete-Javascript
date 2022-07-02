630b5a42035cb5d0de7fba15548146cbb58224fb
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-02 19:56:32 +0530

    converting and checking numbers

---
+++ 09-Numbers-Dates-Timers-Bankist/converting-and-checking-numbers.js
@@ -0,0 +1,9 @@
+'use strict';
+///////////////////////////////////////
+// Converting and Checking Numbers
+console.log(23 === 23.0);
+
+// Base 10 - 0 to 9. 1/10 = 0.1. 3/10 = 3.3333333
+// Binary base 2 - 0 1
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
