b8c33e8f1d8bdc5a828d5608be1c280a954006b3
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-02 21:56:57 +0530

    Math and Rounding in js

---
+++ 09-Numbers-Dates-Timers-Bankist/math-and-rounding.js
@@ -0,0 +1,21 @@
+'use strict';
+///////////////////////////////////////
+// Math and Rounding
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, '23px', 11, 2));
+
+console.log(Math.min(5, 18, 23, 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+// 0...1 -> 0...(max - min) -> min...max
+// console.log(randomInt(10, 20));
