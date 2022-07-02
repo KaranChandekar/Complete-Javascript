86442b01b8ee9f2f8ad00328cb0d181b8c6aafbf
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-02 22:03:47 +0530

    Rounding decimals in practice

--- 09-Numbers-Dates-Timers-Bankist/math-and-rounding.js
+++ 09-Numbers-Dates-Timers-Bankist/math-and-rounding.js
@@ -34,3 +34,9 @@ console.log(Math.trunc(23.3));
 
 console.log(Math.trunc(-23.3));
 console.log(Math.floor(-23.3));
+
+// Rounding decimals
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2));
