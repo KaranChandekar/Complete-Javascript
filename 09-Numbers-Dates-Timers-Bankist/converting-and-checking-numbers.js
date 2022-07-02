c0175893c85de4cc25b6c857a5287b8096794348
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-02 20:18:36 +0530

    parsing numbers in js

--- 09-Numbers-Dates-Timers-Bankist/converting-and-checking-numbers.js
+++ 09-Numbers-Dates-Timers-Bankist/converting-and-checking-numbers.js
@@ -11,3 +11,12 @@ console.log(0.1 + 0.2 === 0.3);
 // Conversion
 console.log(Number('23'));
 console.log(+'23');
+
+// Parsing
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('e23', 10));
+
+console.log(Number.parseInt('  2.5rem  '));
+console.log(Number.parseFloat('  2.5rem  '));
+
+// console.log(parseFloat('  2.5rem  '));
