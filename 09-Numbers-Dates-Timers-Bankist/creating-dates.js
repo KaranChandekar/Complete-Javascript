07f8ab46cf126b55cfd40b2c82b9355dd20d3732
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-03 22:56:56 +0530

    create a date

--- 09-Numbers-Dates-Timers-Bankist/creating-dates.js
+++ 09-Numbers-Dates-Timers-Bankist/creating-dates.js
@@ -1,3 +1,18 @@
 'use strict';
 ///////////////////////////////////////
 // Creating Dates
+
+// Create a date
+
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Aug 02 2020 18:05:41'));
+console.log(new Date('December 24, 2015'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
