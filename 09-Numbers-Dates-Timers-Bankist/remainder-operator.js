2ee9d9996749215140c0fc51d0bcf33a7059f2fb
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-03 15:56:36 +0530

    remainder operator

---
+++ 09-Numbers-Dates-Timers-Bankist/remainder-operator.js
@@ -0,0 +1,19 @@
+'use strict';
+///////////////////////////////////////
+// The Remainder Operator
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
