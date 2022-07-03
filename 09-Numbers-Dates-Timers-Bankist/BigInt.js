e2c45de1bb96b886178c56343578861804e74e5d
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-03 21:33:16 +0530

    how to use BigInt

--- 09-Numbers-Dates-Timers-Bankist/BigInt.js
+++ 09-Numbers-Dates-Timers-Bankist/BigInt.js
@@ -9,3 +9,7 @@ console.log(2 ** 53 + 1);
 console.log(2 ** 53 + 2);
 console.log(2 ** 53 + 3);
 console.log(2 ** 53 + 4);
+
+// Using BigInt
+console.log(4838430248342043823408394839483204n);
+console.log(BigInt(48384302));
