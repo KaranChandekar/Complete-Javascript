979935f411157b0b5cdb7969b0657b6b37b7f316
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-03 22:58:01 +0530

    working with dates

--- 09-Numbers-Dates-Timers-Bankist/creating-dates.js
+++ 09-Numbers-Dates-Timers-Bankist/creating-dates.js
@@ -9,10 +9,30 @@ console.log(now);
 
 console.log(new Date('Aug 02 2020 18:05:41'));
 console.log(new Date('December 24, 2015'));
-console.log(new Date(account1.movementsDates[0]));
+// console.log(new Date(account1.movementsDates[0]));
 
 console.log(new Date(2037, 10, 19, 15, 23, 5));
 console.log(new Date(2037, 10, 31));
 
 console.log(new Date(0));
 console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142256980000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
