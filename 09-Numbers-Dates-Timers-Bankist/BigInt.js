8b17a971d9de9e12ba893992afddf2f6a71b0f4e
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-03 21:39:08 +0530

    Operations with BigInt

--- 09-Numbers-Dates-Timers-Bankist/BigInt.js
+++ 09-Numbers-Dates-Timers-Bankist/BigInt.js
@@ -13,3 +13,12 @@ console.log(2 ** 53 + 4);
 // Using BigInt
 console.log(4838430248342043823408394839483204n);
 console.log(BigInt(48384302));
+
+// Operations
+console.log(10000n + 10000n);
+console.log(36286372637263726376237263726372632n * 10000000n);
+// console.log(Math.sqrt(16n));
+
+const huge = 20289830237283728378237n;
+const num = 23;
+console.log(huge * BigInt(num));
