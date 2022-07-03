a98be57862a2edc50c355d9bc95934b86181f828
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-03 21:32:30 +0530

    max safe integer in js

--- 09-Numbers-Dates-Timers-Bankist/BigInt.js
+++ 09-Numbers-Dates-Timers-Bankist/BigInt.js
@@ -1,3 +1,11 @@
 'use strict';
 ///////////////////////////////////////
 // Working with BigInt
+
+// MAX_SAFE_INTEGER
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
