7dc5407772db6f96aada711eb79a3cb425c1f4c8
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-04 17:34:17 +0530

    fix login date bug

--- 09-Numbers-Dates-Timers-Bankist/script.js
+++ 09-Numbers-Dates-Timers-Bankist/script.js
@@ -190,8 +190,8 @@ btnLogin.addEventListener('click', function (e) {
     const date = `${now.getDate()}`.padStart(2, 0);
     const month = `${now.getMonth() + 1}`.padStart(2, 0);
     const year = now.getFullYear();
-    const hour = now.getHours();
-    const min = now.getMinutes();
+    const hour = `${now.getHours()}`.padStart(2, 0);
+    const min = `${now.getMinutes()}`.padStart(2, 0);
     labelDate.textContent = `${date}/${month}/${year} ${hour}:${min}`;
 
     // Clear input fields
