21bd80e6343bf21a9fc193aef4593d0226dcced8
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-05 11:51:14 +0530

    setTimeout in js

--- 09-Numbers-Dates-Timers-Bankist/timers.js
+++ 09-Numbers-Dates-Timers-Bankist/timers.js
@@ -1,3 +1,16 @@
 'use strict';
 ///////////////////////////////////////
 // Timers
+
+// setTimeout
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => {
+    console.log(`Here is your pizza 🍕 with ${ing1} and ${ing2}`);
+  },
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
