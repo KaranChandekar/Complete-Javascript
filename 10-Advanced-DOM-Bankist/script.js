413a9866ee726e23388eeb377e3e66781be86578
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-06 18:49:51 +0530

    add scrolling effect

--- 10-Advanced-DOM-Bankist/script.js
+++ 10-Advanced-DOM-Bankist/script.js
@@ -135,4 +135,10 @@ btnScrollTo.addEventListener('click', e => {
     document.documentElement.clientHeight,
     document.documentElement.clientWidth
   );
+
+  // Scrolling
+  window.scrollTo(
+    s1coords.left + window.pageXOffset,
+    s1coords.top + window.pageYOffset
+  );
 });
