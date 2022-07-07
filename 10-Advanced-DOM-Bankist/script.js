d5e4c21573b36f9ccc32b00c65f238865fb878e5
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-07 20:00:11 +0530

    comment code

--- 10-Advanced-DOM-Bankist/script.js
+++ 10-Advanced-DOM-Bankist/script.js
@@ -151,14 +151,14 @@ btnScrollTo.addEventListener('click', e => {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-///////////////////////////////////////
-// Types of Events and Event Handlers
-const h1 = document.querySelector('h1');
-const alertH1 = e =>
-  alert('addEventListener: Great! You are reading the heading :D');
-h1.addEventListener('mouseenter', alertH1);
+// ///////////////////////////////////////
+// // Types of Events and Event Handlers
+// const h1 = document.querySelector('h1');
+// const alertH1 = e =>
+//   alert('addEventListener: Great! You are reading the heading :D');
+// h1.addEventListener('mouseenter', alertH1);
 
-setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
-// h1.onmouseenter = e =>
-//   alert('onmouseenter: Great! You just read the heading :D');
+// // h1.onmouseenter = e =>
+// //   alert('onmouseenter: Great! You just read the heading :D');
