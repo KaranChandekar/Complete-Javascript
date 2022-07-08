f18bd5cb0b7467bda1cafca5f0605fa515fefc7a
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-08 19:38:09 +0530

    DOM Traversing - going downwords: child

--- 10-Advanced-DOM-Bankist/script.js
+++ 10-Advanced-DOM-Bankist/script.js
@@ -220,3 +220,10 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 ///////////////////////////////////////
 // DOM Traversing
 const h1 = document.querySelector('h1');
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
