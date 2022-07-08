963175a76b5aaf46bc48791cb9749bc9dd333963
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-08 18:23:37 +0530

    comment code

--- 10-Advanced-DOM-Bankist/script.js
+++ 10-Advanced-DOM-Bankist/script.js
@@ -64,14 +64,14 @@ btnScrollTo.addEventListener('click', e => {
 ///////////////////////////////////////
 // Page navigation
 
-document.querySelectorAll('.nav__link').forEach(el => {
-  el.addEventListener('click', function (e) {
-    e.preventDefault();
-    const id = this.getAttribute('href');
-    console.log(id);
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  });
-});
+// document.querySelectorAll('.nav__link').forEach(el => {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
 
 ///////////////////////////////////////
 ///////////////////////////////////////
