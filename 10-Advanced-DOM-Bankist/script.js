7bcda683c0e84668dfc42ebc88c641cba012be9b
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-08 19:53:25 +0530

    DOM Traversing - going sideways: siblings

--- 10-Advanced-DOM-Bankist/script.js
+++ 10-Advanced-DOM-Bankist/script.js
@@ -235,3 +235,17 @@ console.log(h1.parentElement);
 h1.closest('.header').style.background = 'var(--gradient-secondary)';
 
 h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+// Elements
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+// Nodes
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(el => {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
