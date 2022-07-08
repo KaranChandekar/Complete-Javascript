789f0e09677ba4731b3abb88c574c035cff42cbd
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-08 22:53:38 +0530

    grabbing elements

--- 10-Advanced-DOM-Bankist/script.js
+++ 10-Advanced-DOM-Bankist/script.js
@@ -89,6 +89,9 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 ///////////////////////////////////////
 // Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsConent = document.querySelectorAll('.operations__content');
 
 ///////////////////////////////////////
 ///////////////////////////////////////
