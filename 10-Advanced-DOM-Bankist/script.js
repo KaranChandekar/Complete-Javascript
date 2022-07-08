62e646968bce26ea9cc4fb8e977220b92b1a03be
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-08 19:43:34 +0530

    DOM Traversing - going upwards: parents

--- 10-Advanced-DOM-Bankist/script.js
+++ 10-Advanced-DOM-Bankist/script.js
@@ -227,3 +227,11 @@ console.log(h1.childNodes);
 console.log(h1.children);
 h1.firstElementChild.style.color = 'white';
 h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
