191ca263ae6602444d7994ccb2303ab8ce25efb4
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-10 14:48:57 +0530

    sticky navbar using intersection observer API

--- 10-Advanced-DOM-Bankist/script.js
+++ 10-Advanced-DOM-Bankist/script.js
@@ -153,6 +153,24 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 // const observer = new IntersectionObserver(obsCallback, obsOptions);
 // observer.observe(section1);
 
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyNav = entries => {
+  const [entry] = entries;
+  // console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
 ///////////////////////////////////////
 ///////////////////////////////////////
 ///////////////////////////////////////
