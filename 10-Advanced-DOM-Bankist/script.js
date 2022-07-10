8217cf0d0bc9bf491ad24c79614c4e6d116f5d0b
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-10 19:18:05 +0530

    implement lazy load with intersection observer API

--- 10-Advanced-DOM-Bankist/script.js
+++ 10-Advanced-DOM-Bankist/script.js
@@ -174,6 +174,29 @@ allSections.forEach(function (section) {
 
 ///////////////////////////////////////
 // Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+imgTargets.forEach(img => imgObserver.observe(img));
 
 ///////////////////////////////////////
 ///////////////////////////////////////
