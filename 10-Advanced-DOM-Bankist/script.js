57011a09d794bb93ebb5f8dc271f419ce32a418f
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-11 21:23:20 +0530

    showing acitve slide

--- 10-Advanced-DOM-Bankist/script.js
+++ 10-Advanced-DOM-Bankist/script.js
@@ -218,6 +218,17 @@ const createDots = () => {
 };
 createDots();
 
+const activateDot = function (slide) {
+  document
+    .querySelectorAll('.dots__dot')
+    .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
+};
+activateDot(0);
+
 const goToSlide = slide => {
   slides.forEach((s, i) => {
     s.style.transform = `translateX(${100 * (i - slide)}%)`;
@@ -233,6 +244,7 @@ const nextSlide = () => {
     curSlide++;
   }
   goToSlide(curSlide);
+  activateDot(curSlide);
 };
 
 const prevSlide = () => {
@@ -242,6 +254,7 @@ const prevSlide = () => {
     curSlide--;
   }
   goToSlide(curSlide);
+  activateDot(curSlide);
 };
 
 btnRight.addEventListener('click', nextSlide);
@@ -256,6 +269,7 @@ dotContainer.addEventListener('click', e => {
   if (e.target.classList.contains('dots__dot')) {
     const { slide } = e.target.dataset;
     goToSlide(slide);
+    activateDot(slide);
   }
 });
 
