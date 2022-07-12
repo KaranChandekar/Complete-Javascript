45dbfb5c041b0d31dbbad490205d7041ac7bf756
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-12 11:37:39 +0530

    comment code

--- 10-Advanced-DOM-Bankist/script.js
+++ 10-Advanced-DOM-Bankist/script.js
@@ -469,16 +469,16 @@ slider();
 
 ///////////////////////////////////////
 // Lifecycle DOM Events
-document.addEventListener('DOMContentLoaded', e => {
-  console.log('HTML parsed and DOM tree built!', e);
-});
+// document.addEventListener('DOMContentLoaded', e => {
+//   console.log('HTML parsed and DOM tree built!', e);
+// });
 
-window.addEventListener('load', e => {
-  console.log('Page fully loaded', e);
-});
+// window.addEventListener('load', e => {
+//   console.log('Page fully loaded', e);
+// });
 
-window.addEventListener('beforeunload', e => {
-  e.preventDefault();
-  console.log(e);
-  e.returnValue = '';
-});
+// window.addEventListener('beforeunload', e => {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
