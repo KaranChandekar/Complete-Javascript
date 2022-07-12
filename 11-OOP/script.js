ebef084b5d900b84b642264638066deb21836298
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-12 18:22:55 +0530

    calling construction funtion

--- 11-OOP/script.js
+++ 11-OOP/script.js
@@ -7,3 +7,15 @@ const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
 };
+
+const karan = new Person('Karan', 2003);
+console.log(karan);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const jonas = new Person('jonas', 1991);
+const matilda = new Person('matilda', 1995);
+console.log(jonas, matilda);
