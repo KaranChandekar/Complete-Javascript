3853d687ec2dbe1c37f4184bf5ba18a9fc1a1cbd
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-13 19:32:07 +0530

    implement an accelerate method

--- 11-OOP/coding-challenge-01.js
+++ 11-OOP/coding-challenge-01.js
@@ -20,5 +20,10 @@ const Car = function (make, speed) {
   this.speed = speed;
 };
 
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
 const bmw = new Car('BMW', 120);
 const mercedes = new Car('Mercedes', 95);
