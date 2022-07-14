17211bf1983cda7d76a5c9fcd301b871f6772b5e
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-14 10:27:31 +0530

    set method

--- 11-OOP/setters-and-getters.js
+++ 11-OOP/setters-and-getters.js
@@ -9,5 +9,11 @@ const account = {
   get latest() {
     return this.movements.slice(-1).pop();
   },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
 };
 console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
