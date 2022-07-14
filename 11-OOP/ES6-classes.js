454190ea10557b42e7748db2d50228229da86319
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-14 10:03:05 +0530

    class declaration

--- 11-OOP/ES6-classes.js
+++ 11-OOP/ES6-classes.js
@@ -5,3 +5,14 @@
 
 // Class expression
 const Person = class {};
+
+// Class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+}
+
+const amanda = new PersonCl('Amanda', 1998);
+console.log(amanda);
