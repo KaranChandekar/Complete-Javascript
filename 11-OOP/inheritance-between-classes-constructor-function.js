63b35c36f34dbd21cf8d206195c960af276389d9
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-14 22:34:28 +0530

    inheritance between classes

---
+++ 11-OOP/inheritance-between-classes-constructor-function.js
@@ -0,0 +1,13 @@
+'use strict';
+
+///////////////////////////////////////
+// Inheritance Between "Classes": Constructor Functions
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
