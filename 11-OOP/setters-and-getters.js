1af76b791220b4256f1c94d295263d055c236893
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-14 10:20:20 +0530

    get method

--- 11-OOP/setters-and-getters.js
+++ 11-OOP/setters-and-getters.js
@@ -2,3 +2,12 @@
 
 ///////////////////////////////////////
 // Setters and Getters
+const account = {
+  owner: 'Karan',
+  movements: [200, 150, 300, 420],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+};
+console.log(account.latest);
