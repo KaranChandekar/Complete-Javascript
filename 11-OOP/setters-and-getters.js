8a2f7cf035f0f73854e841c1ca9b17156b77e2ee
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-14 18:54:37 +0530

    set a property that already exists

--- 11-OOP/setters-and-getters.js
+++ 11-OOP/setters-and-getters.js
@@ -17,3 +17,33 @@ const account = {
 console.log(account.latest);
 account.latest = 50;
 console.log(account.movements);
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  get age() {
+    return 2022 - this.birthYear;
+  }
+
+  // Set a property that already exist
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else {
+      alert(`${name} is not a fullname!`);
+    }
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const amanda = new PersonCl('Amanda Davis', 1998);
+const walter = new PersonCl('Walter White', 1995);
+console.log(amanda.age);
+console.log(amanda);
+ 
