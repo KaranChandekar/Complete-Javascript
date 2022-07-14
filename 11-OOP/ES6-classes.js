2655a5791a4bc7208e620d01e0f1975dd7f60a1c
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-14 10:03:44 +0530

    methods with class declaration

--- 11-OOP/ES6-classes.js
+++ 11-OOP/ES6-classes.js
@@ -12,7 +12,24 @@ class PersonCl {
     this.firstName = firstName;
     this.birthYear = birthYear;
   }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2022 - this.birthYear);
+  }
 }
 
 const amanda = new PersonCl('Amanda', 1998);
 console.log(amanda);
+amanda.calcAge();
+
+console.log(amanda.__proto__ === PersonCl.prototype);
+
+PersonCl.prototype.greet = function () {
+  console.log(`Hey ${this.firstName}`);
+};
+amanda.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizes
+// 3. Classes are executed in strict mode
