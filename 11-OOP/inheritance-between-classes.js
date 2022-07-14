325df22184eff5b1e89cf7760f0b58b78ccb6c4d
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-14 23:08:31 +0530

    inheritance between classes in practice

--- 11-OOP/inheritance-between-classes.js
+++ 11-OOP/inheritance-between-classes.js
@@ -11,3 +11,19 @@ const Person = function (firstName, birthYear) {
 Person.prototype.calcAge = function () {
   console.log(2037 - this.birthYear);
 };
+
+const Student = function (firstName, birthYear, course) {
+  Person.call = (this, firstName, birthYear);
+  this.course = course;
+};
+console.log(Student);
+
+Student.prototype.introduce = function () {
+  console.log(
+    `Hey there! My name is ${this.firstName} and persuing ${this.course}`
+  );
+};
+
+const mike = new Student('Mike', 2003, 'Computer Science');
+console.log(mike);
+mike.introduce();
