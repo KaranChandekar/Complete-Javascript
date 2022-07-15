96460aa3d22f94a4ef1fa5bad05f0bc6b4b49685
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-15 08:00:13 +0530

    implement accelerate method with on new constructor function

--- 11-OOP/coding-challenge-03.js
+++ 11-OOP/coding-challenge-03.js
@@ -41,8 +41,17 @@ EV.prototype.chargeBattery = function (chargeTo) {
   this.charge = chargeTo;
 };
 
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+
 const Tesla = new EV('Tesla', 120, 23);
 console.log(Tesla);
 Tesla.chargeBattery(90);
 console.log(Tesla);
 Tesla.brake();
+Tesla.accelerate();
