b1243aab2db8c0767efc41a4dcb36ed53441f0ad
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-15 20:06:51 +0530

    using geolocation api

--- 12-Mapty/script.js
+++ 12-Mapty/script.js
@@ -10,3 +10,17 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+      position = `https://www.google.com/maps/@${longitude},${longitude}`;
+      console.log(position);
+    },
+    function () {
+      alert('Could not get your position!');
+    }
+  );
