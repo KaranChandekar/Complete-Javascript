22217a0a4983dead8d84b44276406552554c7a32
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-15 10:01:37 +0530

    comment code

--- 11-OOP/inheritance-between-classes.js
+++ 11-OOP/inheritance-between-classes.js
@@ -38,63 +38,63 @@
 // Student.prototype.constructor = Student;
 // console.dir(Student.prototype.constructor);
 
-///////////////////////////////////////
-// Inheritance Between "Classes": ES6 Classes
+// ///////////////////////////////////////
+// // Inheritance Between "Classes": ES6 Classes
 
-class PersonCl {
-  constructor(fullName, birthYear) {
-    this.fullName = fullName;
-    this.birthYear = birthYear;
-  }
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
 
-  // Instance methods
-  calcAge() {
-    console.log(2022 - this.birthYear);
-  }
+//   // Instance methods
+//   calcAge() {
+//     console.log(2022 - this.birthYear);
+//   }
 
-  greet() {
-    console.log(`Hey! This is ${fullName}`);
-  }
+//   greet() {
+//     console.log(`Hey! This is ${fullName}`);
+//   }
 
-  get age() {
-    return 2022 - this.birthYear;
-  }
+//   get age() {
+//     return 2022 - this.birthYear;
+//   }
 
-  set fullName(name) {
-    if (name.includes(' ')) this._fullName = name;
-    else alert(`${name} is not a fullname`);
-  }
+//   set fullName(name) {
+//     if (name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not a fullname`);
+//   }
 
-  get fullName() {
-    return this._fullName;
-  }
+//   get fullName() {
+//     return this._fullName;
+//   }
 
-  // Static method
-  static hey() {
-    console.log('Hey there 👋');
-  }
-}
+//   // Static method
+//   static hey() {
+//     console.log('Hey there 👋');
+//   }
+// }
 
-class StudentCl extends PersonCl {
-  constructor(fullName, birthYear, course) {
-    super(fullName, birthYear);
-    this.course = course;
-  }
+// class StudentCl extends PersonCl {
+//   constructor(fullName, birthYear, course) {
+//     super(fullName, birthYear);
+//     this.course = course;
+//   }
 
-  introduce() {
-    console.log(`My name is ${this.fullName} and I study ${this.course}`);
-  }
+//   introduce() {
+//     console.log(`My name is ${this.fullName} and I study ${this.course}`);
+//   }
 
-  calcAge() {
-    console.log(
-      `I'm ${
-        2022 - this.birthYear
-      } years old, but as a student I feel more like ${
-        2022 - this.birthYear + 10
-      }!`
-    );
-  }
-}
-const martha = new StudentCl('Martha jones', 2004, 'Computer Science');
-martha.introduce();
-martha.calcAge();
+//   calcAge() {
+//     console.log(
+//       `I'm ${
+//         2022 - this.birthYear
+//       } years old, but as a student I feel more like ${
+//         2022 - this.birthYear + 10
+//       }!`
+//     );
+//   }
+// }
+// const martha = new StudentCl('Martha jones', 2004, 'Computer Science');
+// martha.introduce();
+// martha.calcAge();
