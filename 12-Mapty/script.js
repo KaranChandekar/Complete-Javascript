6453f0f55059f5eba299931ef2f615e7f058551c
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-15 20:30:13 +0530

    displaying a using leaflet library

--- 12-Mapty/script.js
+++ 12-Mapty/script.js
@@ -19,6 +19,19 @@ if (navigator.geolocation)
       console.log(latitude, longitude);
       position = `https://www.google.com/maps/@${longitude},${longitude}`;
       console.log(position);
+
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position!');
