f1f013175e78d5f740cc4ec0c98fba60215ed439
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-15 11:52:42 +0530

    coding challenge 04

---
+++ 11-OOP/coding-challenge-04.js
@@ -0,0 +1,14 @@
+'use strict';
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
