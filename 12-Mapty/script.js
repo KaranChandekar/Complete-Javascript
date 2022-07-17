72f64997a224aec072b36a1610f4db2923b35725
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-17 10:41:34 +0530

    if workout type is cycling

--- 12-Mapty/script.js
+++ 12-Mapty/script.js
@@ -222,6 +222,21 @@ class App {
           </div>
         </li>
       `;
+
+    if (workout.type === 'cycling')
+      html += `
+          <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⛰</span>
+            <span class="workout__value">${workout.elevationGain}</span>
+            <span class="workout__unit">m</span>
+          </div>
+        </li> 
+      `;
   }
 }
 
