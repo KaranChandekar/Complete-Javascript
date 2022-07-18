7bc1a7758f9711f6d62bccc00ae9acf8c98ad1d0
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-18 22:29:18 +0530

    create request with http

--- 13-Asynchronous/script.js
+++ 13-Asynchronous/script.js
@@ -6,3 +6,4 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 ///////////////////////////////////////
 // Our First AJAX Call: XMLHttpRequest
+const request = XMLHttpRequest();
