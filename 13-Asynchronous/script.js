2bd632ae2cfaa9825db000588ce7c58b7cf718d5
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-18 23:24:40 +0530

    parse response data

--- 13-Asynchronous/script.js
+++ 13-Asynchronous/script.js
@@ -9,8 +9,9 @@ const countriesContainer = document.querySelector('.countries');
 const request = new XMLHttpRequest();
 request.open('GET', 'https://restcountries.com/v3.1/name/india');
 request.send();
-console.log(request.responseText);
 
 request.addEventListener('load', function () {
   console.log(this.responseText);
+  const data = JSON.parse(this.responseText);
+  console.log(data);
 });
