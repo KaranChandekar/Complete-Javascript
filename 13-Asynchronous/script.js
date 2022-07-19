6ba2d1e90f2d50069a73a95488b1cc0b673ecb1d
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-19 22:54:44 +0530

    create html

--- 13-Asynchronous/script.js
+++ 13-Asynchronous/script.js
@@ -11,7 +11,21 @@ request.open('GET', 'https://restcountries.com/v3.1/name/india');
 request.send();
 
 request.addEventListener('load', function () {
-  console.log(this.responseText);
   const data = JSON.parse(this.responseText);
   console.log(data);
+
+  const html = `
+    <article class="country">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages}</p>
+        <p class="country__row"><span>💰</span>${data.currencies}</p>
+        </div>
+    </article>
+  `;
 });
