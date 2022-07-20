ddf2bccfebf56bf8ad7f626086478c842345d241
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-20 20:04:13 +0530

    refactor code

--- 13-Asynchronous/script.js
+++ 13-Asynchronous/script.js
@@ -40,3 +40,38 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 // Welcome to Callback Hell
+
+const renderCountry = function (data) {
+  const html = `
+  <article class="country">
+      <img class="country__img" src="${data.flags.svg}" />
+      <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}M people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages.hin}</p>
+      <p class="country__row"><span>💰</span>${data.currencies.INR.name}</p>
+      </div>
+  </article>
+`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryDataAndNeighbour = function (country) {
+  // Our First AJAX Call: XMLHttpRequest
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    renderCountry(data);
+  });
+};
+
+getCountryDataAndNeighbour('india');
