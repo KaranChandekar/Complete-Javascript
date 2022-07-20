2a0505f41473693c7a137371c580a9b6f52989c8
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-20 20:38:10 +0530

    render neighbour country

--- 13-Asynchronous/script.js
+++ 13-Asynchronous/script.js
@@ -41,9 +41,9 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 // Welcome to Callback Hell
 
-const renderCountry = function (data) {
+const renderCountry = function (data, className = '') {
   const html = `
-  <article class="country">
+  <article class="country ${className}">
       <img class="country__img" src="${data.flags.svg}" />
       <div class="country__data">
       <h3 class="country__name">${data.name.common}</h3>
@@ -61,8 +61,8 @@ const renderCountry = function (data) {
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryDataAndNeighbour = function (country) {
-  // Our First AJAX Call: XMLHttpRequest
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
   request.send();
@@ -70,8 +70,26 @@ const getCountryDataAndNeighbour = function (country) {
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
+
+    // Render country 1
     renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryDataAndNeighbour('india');
+getCountryAndNeighbour('india');
