e8cc841f58cb97d9ac15021cb0b9a8e1a8b8aa8c
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-21 09:27:37 +0530

    simplify fetch code

--- 13-Asynchronous/script.js
+++ 13-Asynchronous/script.js
@@ -114,15 +114,21 @@ const renderCountry = function (data, className = '') {
 //   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
 //   request.send();
 
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(function (response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      renderCountry(data[0]);
-    });
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
 };
 getCountryData('india');
