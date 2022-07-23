6f01c89d394fd904be7dd255f5253300ee58cf0a
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-23 16:09:42 +0530

    event loop

--- 13-Asynchronous/script.js
+++ 13-Asynchronous/script.js
@@ -252,3 +252,16 @@ whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
 */
+
+///////////////////////////////////////
+// The Event Loop in Practice
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end');
