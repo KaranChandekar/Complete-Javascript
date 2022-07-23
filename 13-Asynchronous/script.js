ac59efeb1372b84a0284afc7414af02799900cc0
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-23 16:08:58 +0530

    comment code

--- 13-Asynchronous/script.js
+++ 13-Asynchronous/script.js
@@ -227,6 +227,7 @@ TEST COORDINATES 2: -33.933, 18.474
 GOOD LUCK 😀
 */
 
+/*
 const whereAmI = function (lat, lng) {
   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
     .then(res => {
@@ -250,3 +251,4 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+*/
