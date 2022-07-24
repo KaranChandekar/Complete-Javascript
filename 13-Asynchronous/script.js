a66f873a30c0fe0799fb0e90ece28b934b6c722a
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-24 11:07:04 +0530

    solved step 2

--- 13-Asynchronous/script.js
+++ 13-Asynchronous/script.js
@@ -414,3 +414,25 @@ const createImage = function (imgPath) {
     });
   });
 };
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));
