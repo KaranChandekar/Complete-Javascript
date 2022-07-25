eb9288dc632c968810847764f9e62845c5a927e7
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-25 20:16:46 +0530

    solved part 01

--- 13-Asynchronous/script.js
+++ 13-Asynchronous/script.js
@@ -692,3 +692,23 @@ let currentImg;
 //     currentImg.style.display = 'none';
 //   })
 //   .catch(err => console.error(err));
+
+// PART 1
+const loadNPause = async function () {
+  try {
+    // Load image 1
+    let img = await createImage('img/img-1.jpg');
+    console.log('Image 1 loaded');
+    await wait(2);
+    img.style.display = 'none';
+
+    // Load image 1
+    img = await createImage('img/img-2.jpg');
+    console.log('Image 2 loaded');
+    await wait(2);
+    img.style.display = 'none';
+  } catch (err) {
+    console.error(err);
+  }
+};
+// loadNPause();
