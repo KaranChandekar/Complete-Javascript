9fd66e5e022541190eb55a13685e8a818cb151b0
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-26 14:05:25 +0530

    module pattern

--- 14-Modern-JS-Modules-Tooling/script.js
+++ 14-Modern-JS-Modules-Tooling/script.js
@@ -1,6 +1,7 @@
 ///////////////////////////////////////
 // Exporting and Importing in ES6 Modules
 
+/*
 // Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // addToCart('bread', 4);
@@ -23,3 +24,36 @@ add('apples', 3);
 // imports are not copies of the exports they are instead a live connection
 import { cart } from './shoppingCart.js';
 console.log(cart);
+*/
+
+///////////////////////////////////////
+// The Module Pattern
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 263;
+  const totalQuantity = 53;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apples', 6);
+ShoppingCart2.addToCart('pizza', 3);
+console.log(ShoppingCart2);
