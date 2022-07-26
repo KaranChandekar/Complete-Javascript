baf8b34e5ead4580f14c8c0214f06f55ab37695b
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-26 22:10:18 +0530

    babel and polyfilling

--- 14-Modern-JS-Modules-Tooling/script.js
+++ 14-Modern-JS-Modules-Tooling/script.js
@@ -97,3 +97,24 @@ console.log(stateDeepClone);
 if (module.hot) {
   module.hot.accept();
 }
+
+class Person {
+  #greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+const jonas = new Person('Jonas');
+
+console.log('Jonas' ?? null);
+
+console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
+
+// Polifilling async functions
+import 'regenerator-runtime/runtime';
