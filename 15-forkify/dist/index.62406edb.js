a1f7b5295783175b9374079f23ac9877d061fd9a
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-27 19:28:44 +0530

    throwing error

--- 15-forkify/dist/index.62406edb.js
+++ 15-forkify/dist/index.62406edb.js
@@ -12,6 +12,7 @@ const showRecipe = async function() {
     try {
         const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
         const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
         console.log(res, data);
     } catch (err) {
         alert(err);
