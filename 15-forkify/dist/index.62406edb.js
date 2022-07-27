489c6dd3b4aebe91c66a55a9aa5117a5b58a95f4
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-27 19:59:05 +0530

    format recipe data

--- 15-forkify/dist/index.62406edb.js
+++ 15-forkify/dist/index.62406edb.js
@@ -13,7 +13,18 @@ const showRecipe = async function() {
         const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
         const data = await res.json();
         if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-        console.log(res, data);
+        let { recipe  } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
     } catch (err) {
         alert(err);
     }
--- 15-forkify/src/js/controller.js
+++ 15-forkify/src/js/controller.js
@@ -21,7 +21,18 @@ const showRecipe = async function () {
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
 
-    console.log(res, data);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
   } catch (err) {
     alert(err);
   }
