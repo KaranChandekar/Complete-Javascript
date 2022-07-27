929fca893ade20cf568567cfb2770ed4b65cf08c
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-27 19:13:07 +0530

    fetch api and use try catch

--- 15-forkify/dist/index.62406edb.js
+++ 15-forkify/dist/index.62406edb.js
@@ -5,7 +5,15 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+    } catch (err) {
+        alert(err);
+    }
+};
 
 //# sourceMappingURL=index.62406edb.js.map
--- 15-forkify/src/js/controller.js
+++ 15-forkify/src/js/controller.js
@@ -11,3 +11,13 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+  } catch (err) {
+    alert(err);
+  }
+};
